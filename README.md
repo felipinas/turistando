0cdddc3885a553e044bddf5e8253d6e98358ee23
João Felipe <jfss2@cin.ufpe.br>
2020-06-23 02:18:59 -0300

    adição de um galeria.
    além de uma pequena mudança no readme, houve adição de uma galeria para fotos dos ponto turísticos, com o objetivo de oferecer mais opções pro usuário.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ![Logo Turistando](https://github.com/felipinas/projeto-turistando/blob/master/imagens/logofooter.png) 
 
-Sabemos que vários setores do Brasil foram afetados pela pandemia da COVID-19, como o turismo. Partindo desse ponto e com a enorme vontade de amenizar consequências dessa situação, surge o Turistando: **projeto que incentiva, inicialmente, o turismo pelo estado de Pernambuco, trazendo a reflexão de como o país é belo e reunindo vários lugares do estado.**
+Sabemos que vários setores do Brasil foram afetados pela pandemia da COVID-19, como o turismo. Partindo desse ponto e com a enorme vontade de amenizar consequências dessa situação, surge o Turistando: **projeto que incentiva o turismo, inicialmente, pela Região Metropolitana do Recife, trazendo a reflexão de como o país é belo e reunindo vários lugares do estado.**
 
 Se liga que maneiro: além de ver lugares cheios de encanto, **você ajuda famílias que dependem do turismo local, antes afetadas pela pandemia.**
 
--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+//funcao para menu hamburguer
+
 function openMenu() {
     //Abrir o menu no mobile
 
@@ -15,12 +17,11 @@ function openMenu() {
     }
 }
 
+//funcao para abrir os textos dos lugares
+
 function openTexto(id) {
     var display = document.getElementById(`${id}`).style.display
 
-    var botao = document.getElementsByClassName('button-mais')
-
-    console.log(botao)
 
     if (display == 'none') {
         document.getElementById(`${id}`).style.display = 'block'
@@ -28,4 +29,27 @@ function openTexto(id) {
         document.getElementById(`${id}`).style.display = 'none'
     }
   
+}
+
+//funcoes para a galeria 
+
+var min = 1
+var max = 3
+var fi = min //imagem indice
+
+function proxFoto(idFoto, pastaFoto) { 
+    fi++
+    if (fi > 3) {
+        fi = min
+    }
+    console.log(`${idFoto}`)
+    document.getElementById(`${idFoto}`).src = `imagens/olinda/${fi}.png`
+}
+
+function anteFoto(idFoto, pastaFoto) {
+    fi--
+    if (fi < 1) {
+        fi = max
+    }
+    document.getElementById(`${idFoto}`).src = `imagens/olinda/${fi}.png`
 }
